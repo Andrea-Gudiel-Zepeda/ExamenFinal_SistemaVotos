6cc2be9b36bd8122423a18fd3cd8865babff9275
Andrea-Gudiel-Zepeda <andreamaria1d@gmail.com>
2023-05-05 21:02:26 -0600

    cambios

--- MVC_SVotos/Models/SistemaVotosContext.cs
+++ MVC_SVotos/Models/SistemaVotosContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace MVC_SVotos.Models;
+namespace ExamenFinal_SistemaVotos.Models;
 
 public partial class SistemaVotosContext : DbContext
 {
